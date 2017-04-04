9d4894826348d22af58dfaa0a07b8ba07bcf1f94
Fredrik Feyling <fredrikfeyling@Fredriks-MacBook-Pro.local>
2017-04-04 10:18:38 +0200

    Update init.sh and instructions

--- init.sh
+++ init.sh
@@ -1,10 +1,13 @@
-if [! -d .oh-my-zsh]
+if [ ! -d $HOME/.oh-my-zsh ]
 then 
+	echo "Installing oh-my-zsh..."
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else 
+echo "oh-my-zsh was already installed"
 fi
 ln -s .dotfiles/.vimrc $HOME/.vimrc
 ln -s .dotfiles/.zshrc $HOME/.zshrc
-rm $ZSH_CUSTOM/aliases.zsh
+if [ -f $ZSH_CUSTOM/aliases.zsh ]; then; rm $ZSH_CUSTOM/aliases.zsh;
 ln -s /$HOME/.dotfiles/zsh_custom/aliases.zsh $ZSH_CUSTOM/aliases.zsh
 source .zshrc
 echo "oh-my-zsh installed. Symlinked .vimrc and .zshrc from home filder, and aliases.zsh to correct zsh folder. Sourced .zshrc" 
