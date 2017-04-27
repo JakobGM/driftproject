0e53c8cbe2c2b8d790eb288437cbcf7a8f840fb4
Fredrik Feyling <fredrikfeyling@Fredriks-MacBook-Pro.local>
2017-04-27 17:50:37 +0200

    Create initialization script.

--- solutions/fredrief/init.sh
+++ solutions/fredrief/init.sh
@@ -1,16 +1,33 @@
+#!/bin/zsh
+ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
+#Check if oh-my-zsh is already installed and install it if it is not. 
 if [ ! -d $HOME/.oh-my-zsh ]
 then 
 	echo "Installing oh-my-zsh..."
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else 
-echo "oh-my-zsh was already installed"
+    echo "oh-my-zsh was already installed"
 fi
-ln -s .dotfiles/.vimrc $HOME/.vimrc
-ln -s .dotfiles/.zshrc $HOME/.zshrc
-if [ -f $ZSH_CUSTOM/aliases.zsh ]
-then
-	rm $ZSH_CUSTOM/aliases.zsh
+
+# Glob hidden files
+setopt glob_dots
+
+# Get location of dotfiles repo
+# Important assumption: It is located at $HOME/.dotfiles
+repo=$HOME/.dotfiles
+echo "----------------------"
+echo "Repo name: ${repo}"
+
+# Symlink all home directory dotfiles
+ln -svi $repo/home/* $HOME
+
+# Symlink all ZSH_CUSTOM files
+ln -svi $repo/zsh_custom/* $HOME/.oh-my-zsh/custom
+
+# Source zsh config if the shell is zsh
+if [ ! -z $ZSH ]; then
+            source $HOME/.zshrc
 fi
-ln -s /$HOME/.dotfiles/zsh_custom/aliases.zsh $ZSH_CUSTOM/aliases.zsh
-source $HOME/.zshrc
+
 echo "oh-my-zsh installed. Symlinked .vimrc and .zshrc from home filder, and aliases.zsh to correct zsh folder. Sourced .zshrc" 
+
